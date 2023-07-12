b08c40e04c583f6d21bf38b81e3f3dce7741ff16
teriyaki0 <ttemyrlan5@gmail.com>
2023-07-12 16:34:11 +0600

    fix1

--- api.ts
+++ api.ts
@@ -1,69 +1,108 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { ITask } from "./types/tasks";
 import moment from "moment";
 
 axios.defaults.baseURL = process.env.API_URL || "http://localhost:3001";
 
-
 export const getOnProgressTask = async (): Promise<ITask[]> => {
-  const { data } = await axios.get("/tasks", {
-    headers: {
-      "Cache-Control": "no-cache",
-      Pragma: "no-cache",
-      Expires: "0",
-    },
-  });
+  try {
+    const { data } = await axios.get("/tasks", {
+      headers: {
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
+    });
 
-  const filteredTasks = data.filter((task: ITask) => !task.isCompleted);
+    const filteredTasks = data.filter((task: ITask) => !task.isCompleted);
 
-  filteredTasks.sort((a: ITask, b: ITask) => {
-    const dateA = moment(a.date);
-    const dateB = moment(b.date);
-    return dateA.diff(dateB);
-  });
+    filteredTasks.sort((a: ITask, b: ITask) => {
+      const dateA = moment(a.date);
+      const dateB = moment(b.date);
+      return dateA.diff(dateB);
+    });
 
-  return filteredTasks;
+    return filteredTasks;
+  } catch (error) {
+    handleRequestError(error);
+    throw error;
+  }
 };
 
 export const getOnCompletedTask = async (): Promise<ITask[]> => {
-  const { data } = await axios.get("/tasks", {
-    headers: {
-      "Cache-Control": "no-cache",
-      Pragma: "no-cache",
-      Expires: "0",
-    },
-  });
-  const filteredTasks = data.filter((task: ITask) => task.isCompleted);
-  return filteredTasks;
+  try {
+    const { data } = await axios.get("/tasks", {
+      headers: {
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
+    });
+
+    const filteredTasks = data.filter((task: ITask) => task.isCompleted);
+    return filteredTasks;
+  } catch (error) {
+    handleRequestError(error);
+    throw error;
+  }
 };
 
 export const addTask = async (data: ITask): Promise<ITask> => {
-  const newTodo = await axios.post("/tasks", data, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  return newTodo.data;
+  try {
+    const newTodo = await axios.post("/tasks", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return newTodo.data;
+  } catch (error) {
+    handleRequestError(error);
+    throw error;
+  }
 };
 
 export const editTask = async (data: ITask): Promise<ITask> => {
-  const res = await axios.put(`/tasks/${data.id}`, data, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  return res.data;
+  try {
+    const res = await axios.put(`/tasks/${data.id}`, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    handleRequestError(error);
+    throw error;
+  }
 };
 
 export const completedTask = async (data: ITask): Promise<ITask> => {
-  const res = await axios.put(`/tasks/${data.id}`, data, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  return res.data;
+  try {
+    const res = await axios.put(`/tasks/${data.id}`, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    handleRequestError(error);
+    throw error;
+  }
 };
 
 export const deleteTask = async (id: number): Promise<void> => {
-  await axios.delete(`/tasks/${id}`);
+  try {
+    await axios.delete(`/tasks/${id}`);
+  } catch (error) {
+    handleRequestError(error);
+    throw error;
+  }
+};
+
+const handleRequestError = (error: unknown) => {
+  if (axios.isAxiosError(error)) {
+    const axiosError = error as AxiosError;
+    console.error("Request Error:", axiosError.response?.data);
+  } else {
+    console.error("Unknown Error:", error);
+  }
 };
